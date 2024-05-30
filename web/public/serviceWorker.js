39f78f6df971c5ba92a8c04d408604a3f8282f1b
Michael Johnson <michael@icefoganalytics.com>
2024-05-30 13:11:44 -0700

    Update serviceWorker.js

--- web/public/serviceWorker.js
+++ web/public/serviceWorker.js
@@ -2,18 +2,25 @@
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
-self.addEventListener('message', event => {
-  console.log(event);
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+self.addEventListener("message", (event) => {
+  console.log("MESSAGE", event);
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
 
-console.log("Loading serviceWorker.js")
+self.addEventListener("fetch", (event) => {
+  console.log("FETCH", event);
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+console.log("Loading serviceWorker.js");
 
 //workbox.clientsClaim();
 
 // The precaching code provided by Workbox.
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+//self.__precacheManifest = [].concat(self.__precacheManifest || []);
+//workbox.precaching.suppressWarnings();
+//workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
