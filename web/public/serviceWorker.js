c8fe77d54f89a86902effc3355d23d9f0aa9cca8
Caleb Burke <calebburke91@gmail.com>
2024-06-19 00:15:53 -0700

    Caching images and stylesheets, caching js scripts causes issues and needs more work

--- web/public/serviceWorker.js
+++ web/public/serviceWorker.js
@@ -1,14 +1,77 @@
 /* eslint-disable no-undef, no-restricted-globals */
 
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 workbox.setConfig({
   debug: true,
 });
 
-workbox.precaching.precacheAndRoute([
+/* skipWaiting() from workbox-core is no longer recommended and will be removed in Workbox v7. Using self.skipWaiting() instead is equivalent. */
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+const { strategies } = workbox;
+const { expiration } = workbox;
+
+/* workbox.precaching.precacheAndRoute([
+  { url: '/index.html', revision: '000000' },
+]);
+ */
+
+// Cache scripts
+// TODO: 'Report issue' button functionality breaks when script's are cached, fix this issue
+/* workbox.routing.registerRoute(({ request }) => {
+  return request.destination === 'script';
+},
+  new strategies.CacheFirst({
+    cacheName: 'js-cache',
+    plugins: [
+      // Use the ExpirationPlugin to manage the cache size and age
+      new expiration.ExpirationPlugin({
+        // Set the maximum number of entries (files) that can be stored in the cache
+        maxEntries: 255,
+
+        // Set the maximum age of an entry in the cache (in seconds)
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+); */
+
+// Cache stylesheets (css)
+// TODO: Figure out what this cache is always empty
+workbox.routing.registerRoute(({ request }) => {
+  return request.destination === 'style';
+},
+  new strategies.CacheFirst({
+    cacheName: 'css-cache',
+    plugins: [
+      new expiration.ExpirationPlugin({
+        maxEntries: 8,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+
+// Cache images
+workbox.routing.registerRoute(({ request }) => {
+  console.log('Request destination:', request.destination);
+  return request.destination === 'image';
+},
+  new strategies.CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new expiration.ExpirationPlugin({
+        maxEntries: 8,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+/* workbox.precaching.precacheAndRoute([
   {url: '/yukon.svg', revision: '123456'}, // Change the revision to force update
 ]);
 
@@ -20,7 +83,7 @@ self.addEventListener("message", (event) => {
     self.skipWaiting();
   }
 });
-
+ */
 /*
 self.addEventListener("fetch", (event) => {
    
