13c9fbdf4b07063044da03823991cff71a7ea084
Michael Johnson <michael@icefoganalytics.com>
2024-07-30 11:29:31 -0700

    Ignore latest migration

--- api/src/data/migrations/001.create-users-table.ts
+++ api/src/data/migrations/001.create-users-table.ts
@@ -1,29 +1,26 @@
 import * as knex from "knex";
 
 export async function up(knex: knex.Knex) {
-    await knex.schema.createTable("users", function(table) {
-        table.increments("id").notNullable().primary();
-        table.string("email", 250).notNullable();
-        table.string("auth_subject", 250).notNullable();
-        table.string("first_name", 100);
-        table.string("last_name", 100);
-        table.string("display_name", 200);
-        table.string("title", 100);
-        table.string("department", 100);
-        table.string("division", 100);
-        table.string("branch", 100);
-        table.string("unit", 100);
-        table.boolean("is_active").defaultTo(true).notNullable();
+  await knex.schema.createTable("users", function (table) {
+    table.increments("id").notNullable().primary();
+    table.string("email", 250).notNullable();
+    table.string("auth_subject", 250).notNullable();
+    table.string("first_name", 100);
+    table.string("last_name", 100);
+    table.string("display_name", 200);
+    table.string("title", 100);
+    table.string("department", 100);
+    table.string("division", 100);
+    table.string("branch", 100);
+    table.string("unit", 100);
+    table.boolean("is_active").defaultTo(true).notNullable();
 
-        table.unique(["email"], {
-            indexName: "unique_users_email",
-        });
-        table.unique(["auth_subject"], {
-            indexName: "unique_users_auth_subject",
-        });
+    table.unique(["email"], {
+      indexName: "unique_users_email",
     });
-};
+  });
+}
 
 export async function down(knex: knex.Knex) {
-    await knex.schema.dropTable("users");
-};
+  await knex.schema.dropTable("users");
+}
