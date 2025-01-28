5b62cbee011e4c96ad0064561b25b76c3b9158bb
Michael Johnson <michael@icefoganalytics.com>
2025-01-27 17:14:04 -0700

    Better Rending on mobile

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,4 @@
-export const applicationName = "YG Safety Portal";
+export const applicationName = "Safety Portal";
 
 export const environment = process.env.NODE_ENV;
 export const apiBaseUrl = process.env.NODE_ENV == "production" ? "" : "http://localhost:3000";
--- web/src/layouts/DefaultNoAuth.vue
+++ web/src/layouts/DefaultNoAuth.vue
@@ -1,8 +1,10 @@
 <template>
   <v-app-bar app color="#fff" flat height="70" style="left: 0; border-bottom: 3px #f3b228 solid">
-    <router-link to="/"><img src="/yukon.svg" style="margin: -10px 85px 0 14px" height="44" /></router-link>
+    <router-link to="/"
+      ><img src="/yukon.svg" style="margin-top: -10px; margin-left: 14px" height="44" class="mr-0 mr-md-6"
+    /></router-link>
     <!-- <v-img class="ml-0m pl-0" src="src/assets/yukon.svg" height="44" /> -->
-    <v-app-bar-title class="pt-0 font-weight-bold" style="margin-left: -20px">
+    <v-app-bar-title class="pt-0 font-weight-bold">
       <router-link to="/" class="title-link">{{ applicationName }}</router-link>
     </v-app-bar-title>
 
@@ -11,7 +13,7 @@
         <!--  <v-btn color="primary" class="mr-1" to="/administration" icon="mdi-home"></v-btn> -->
 
         <v-divider class="mr-5" vertical inset></v-divider>
-        <span style="font-size: 0.9rem"> {{ user.display_name }} </span>
+        <span class="d-none d-md-inline" style="font-size: 0.9rem">{{ user.display_name }}</span>
 
         <v-menu offset-y>
           <template v-slot:activator="{ props }">
@@ -19,6 +21,11 @@
           </template>
 
           <v-list density="compact">
+            <v-list-item class="d-flex d-md-none">
+              <template v-slot:prepend> <v-icon>mdi-account</v-icon></template>
+              <v-list-item-title style="font-size: 0.9rem !important">{{ user.display_name }}</v-list-item-title>
+            </v-list-item>
+
             <v-list-item to="/hazard-library">
               <template v-slot:prepend>
                 <v-icon>mdi-hazard-lights</v-icon>
