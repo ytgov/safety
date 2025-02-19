e781578c30f3081ae13df2d81900cabd04678d83
Michael Johnson <michael@icefoganalytics.com>
2025-02-19 12:05:08 -0700

    Editing locations

--- api/src/middleware/index.ts
+++ api/src/middleware/index.ts
@@ -1,12 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
+import { isNil } from "lodash";
+import { UserRole } from "../data/models";
 
 export async function ReturnValidationErrors(req: Request, res: Response, next: NextFunction) {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    next();
+  next();
+}
+
+export async function RequireAdmin(req: Request, res: Response, next: NextFunction) {
+  if (isNil(req.user.roles || req.user.roles.length == 0)) {
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  if (!req.user.roles.filter((role: UserRole) => role.name === "System Admin").length) {
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  next();
 }
--- api/src/routes/location-router.ts
+++ api/src/routes/location-router.ts
@@ -1,10 +1,36 @@
 import express, { Request, Response } from "express";
 
 import { db as knex } from "../data";
+import { RequireAdmin } from "../middleware";
+import { checkJwt, loadUser } from "../middleware/authz.middleware";
 
 export const locationRouter = express.Router();
 
 locationRouter.get("/", async (_req: Request, res: Response) => {
-  const list = await knex("locations");
+  const list = await knex("locations").orderBy("name", "asc");
   res.json({ data: list });
+});
+
+locationRouter.post("/", checkJwt, loadUser, RequireAdmin, async (req: Request, res: Response) => {
+  const { code, name, description } = req.body;
+
+  try {
+    const list = await knex("locations").insert({ code, name, description });
+
+    res.json({ data: list });
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
+
+locationRouter.put("/:code", async (req: Request, res: Response) => {
+  const { code } = req.params;
+  const { name, description } = req.body;
+
+  try {
+    const list = await knex("locations").where({ code }).update({ name, description });
+    res.json({ data: list });
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
 });
