96720b8b45bff81b5ae8f916d6873c137597fa6d
Michael Johnson <michael@icefoganalytics.com>
2025-03-13 09:31:11 -0700

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -40,7 +40,7 @@ app.use(
       "frame-ancestors": ["'self'"],
       "img-src": ["'self'", "data:"],
       "object-src": ["'none'"],
-      "script-src": ["'self'", "'unsafe-eval'", "https://storage.googleapis.com"],
+      "script-src": ["'self'", "'unsafe-eval'", "'unsafe-inline'", "https://storage.googleapis.com"],
       "script-src-attr": ["'none'"],
       "style-src": ["'self'", "https:", "'unsafe-inline'"],
       "worker-src": ["'self'", "blob:"],
