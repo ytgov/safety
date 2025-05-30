5c187b48a994a6a9979fa3f33b0481160d4c36f6
Michael Johnson <michael@icefoganalytics.com>
2025-05-30 16:41:35 -0700

    auth config for uat

--- web/src/plugins/auth.ts
+++ web/src/plugins/auth.ts
@@ -1,9 +1,10 @@
 import { createAuth0 } from "@auth0/auth0-vue";
-import { production, development } from "../../auth-config.json";
+import { production, uat, development } from "../../auth-config.json";
 
 let config = production;
 
 if (window.location.host == "localhost:8080") config = development;
+else if (window.location.host == "safety-test.gov.yk.ca") config = uat;
 
 export default createAuth0({
   domain: config.domain,
