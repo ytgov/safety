e2820801253e32768417bdcf8487510d04e31a06
laurian_li <laurian.li@mail.utoronto.ca>
2025-07-31 13:19:36 -0700

    Renamed seeding

--- api/src/data/seeds/016.data-ingestion-mappings-airport.ts
+++ api/src/data/seeds/016.data-ingestion-mappings-airport.ts
@@ -93,10 +93,10 @@ export async function seed(knex: knex.Knex) {
   ];
 
   for (const row of mappings) {
-    const exists = await knex("data_injection_mappings").where(row).first();
+    const exists = await knex("data_ingestion_mappings").where(row).first();
 
     if (!exists) {
-      await knex("data_injection_mappings").insert(row);
+      await knex("data_ingestion_mappings").insert(row);
     }
   }
 }
