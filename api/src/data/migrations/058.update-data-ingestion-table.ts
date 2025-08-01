8b74f0d5c5ad2e0ea4db2444ecebfef9473caa38
laurian_li <laurian.li@mail.utoronto.ca>
2025-08-01 11:45:45 -0700

    fix 54 migration problem

--- api/src/data/migrations/058.update-data-ingestion-table.ts
+++ api/src/data/migrations/058.update-data-ingestion-table.ts
@@ -1,7 +1,7 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable("data_injections", (table) => {
+  await knex.schema.alterTable("data_ingestions", (table) => {
     table.text("description").nullable().alter();
     table.text("description_moderated").nullable().alter();
     table.text("location_detail").nullable().alter();
@@ -9,7 +9,7 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable("data_injections", (table) => {
+  await knex.schema.alterTable("data_ingestions", (table) => {
     table.string("description", 255).notNullable().alter();
     table.string("description_moderated", 255).notNullable().alter();
     table.string("location_detail", 255).notNullable().alter();
