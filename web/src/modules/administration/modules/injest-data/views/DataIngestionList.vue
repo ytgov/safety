779c58208f27a1a53cd436cd236ee09e184bfe39
Michael Johnson <michael@icefoganalytics.com>
2025-08-05 19:01:40 -0700

    Formatting changes to upload

--- web/src/modules/administration/modules/injest-data/views/DataIngestionList.vue
+++ web/src/modules/administration/modules/injest-data/views/DataIngestionList.vue
@@ -5,32 +5,24 @@
     style="margin: -13px -16px 10px -16px"
     class="pl-4 mb-4"
     color="white"
-    active-color="#fff"
-  >
+    active-color="#fff">
     <template v-slot:prepend>
-      <v-icon
-        color="white"
-        icon="mdi-home"
-      ></v-icon>
+      <v-icon color="white" icon="mdi-home"></v-icon>
     </template>
     <template v-slot:divider>
-      <v-icon
-        color="white"
-        icon="mdi-chevron-right"
-      ></v-icon>
+      <v-icon color="white" icon="mdi-chevron-right"></v-icon>
     </template>
   </v-breadcrumbs>
 
   <h1>Injest-Data</h1>
-  <div class="d-flex justify-center w-100">
-    <v-btn
-      color="primary"
-      variant="flat"
-      size="large"
-      class="mr-5"
-      @click="showEditor = true"
-    >
-      Add Data
+
+  <p>
+    This process loads a CSV file into the system from external sources for reporting purposes. The uploaded data will
+    not be available or visible within this application.
+  </p>
+  <div class="">
+    <v-btn color="primary" variant="flat" size="large" class="mr-5" @click="showEditor = true">
+      Upload Data File
     </v-btn>
   </div>
 
