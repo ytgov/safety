38320fe717df1047a5a315cde17f2f14e56ddfa3
Michael Johnson <michael@icefoganalytics.com>
2025-10-22 10:50:42 -0700

    Fix the bug where unknown dept goes to CS

--- api/src/services/department-service.ts
+++ api/src/services/department-service.ts
@@ -10,20 +10,29 @@ export class DepartmentService {
     const userResults = await directory.searchByEmail(userEmail);
     const superResults = await directory.searchByEmail(supervisorEmail);
 
-    let department = await db("departments").first();
-
     if (userResults && userResults.length > 0) {
       await db("users").where({ email: userEmail }).update({ department: userResults[0].department });
       const userDepart = await db("departments").where({ name: userResults[0].department }).first<Department>();
-      if (userDepart) department = userDepart;
+      if (userDepart) return userDepart;
     }
 
     if (superResults && superResults.length > 0) {
       await db("users").where({ email: supervisorEmail }).update({ department: superResults[0].department });
       const superDepart = await db("departments").where({ name: superResults[0].department }).first<Department>();
-      if (superDepart) department = superDepart;
+      if (superDepart) return superDepart;
     }
 
+    if (userEmail.endsWith("@wcb.yk.ca") || supervisorEmail.endsWith("@wcb.yk.ca")) {
+      const wcbDepart = await db("departments").where({ code: "WSCB" }).first<Department>();
+      if (wcbDepart) return wcbDepart;
+    }
+
+    if (userEmail.endsWith("@yesnet.yk.ca") || supervisorEmail.endsWith("@yesnet.yk.ca")) {
+      const eduDepart = await db("departments").where({ code: "EDU" }).first<Department>();
+      if (eduDepart) return eduDepart;
+    }
+
+    let department = await db("departments").where({ code: "UNK" }).first();
     return department;
   }
 }
