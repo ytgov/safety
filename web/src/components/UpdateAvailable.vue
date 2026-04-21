3b450a78c74794e97950e50422fd7384712ff38d
Michael Johnson <michael@icefoganalytics.com>
2026-04-21 11:32:44 -0700

    Change update message from Safety to STICH
    Update the text in UpdateAvailable.vue so the update snackbar displays the correct product name (STICH) instead of "Safety". No functional changes.

--- web/src/components/UpdateAvailable.vue
+++ web/src/components/UpdateAvailable.vue
@@ -1,7 +1,7 @@
 <template>
   <v-snackbar v-model="visible" :timeout="-1" color="primary" location="bottom right">
     <v-icon class="mr-3">mdi-update</v-icon>
-    A new version of Safety is available.
+    A new version of STICH is available.
     <template #actions>
       <v-btn variant="text" @click="reload">Reload</v-btn>
       <v-btn variant="text" @click="dismiss">Dismiss</v-btn>
